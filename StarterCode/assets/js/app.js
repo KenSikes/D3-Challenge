e3a7047c6d42d6377d19aac9aa1cf44b6438ad67
Ken <kensikes988@gmail.com>
2021-06-01 19:34:13 -0700

    saving

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,14 @@
 // @TODO: YOUR CODE HERE!
+
+// Set X & Y axis variables.
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+function xScale(data, chosenXAxis, chartWidth) {
+    // Create scales.
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[chosenXAxis]) * .8,
+            d3.max(data, d => d[chosenXAxis]) * 1.1])
+        .range([0, chartWidth]);
+    return xLinearScale;
+}
