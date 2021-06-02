717ae8b2263282c916e36f210b0f8112a83ab968
Ken <kensikes988@gmail.com>
2021-06-01 19:56:28 -0700

    saving

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -12,3 +12,29 @@ function xScale(data, chosenXAxis, chartWidth) {
         .range([0, chartWidth]);
     return xLinearScale;
 }
+
+// Function for updating x axis
+function renderXAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+    return xAxis;
+}
+// Function for updating y scale
+function yScale(data, chosenYAxis, chartHeight) {
+    // Create scales.
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[chosenYAxis]) * .8,
+            d3.max(data, d => d[chosenYAxis]) * 1.2])
+        .range([chartHeight, 0]);
+    return yLinearScale;
+}
+// Function used for updating y axis
+function renderYAxes(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+    yAxis.transition()
+        .duration(1000)
+        .call(leftAxis);
+    return yAxis;
+}
